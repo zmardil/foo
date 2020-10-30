88dd341957d9cb2cf3a6d152db798d92f74a2633
zmardil <btzardil@gmail.com>
2020-10-30 23:39:50 +0530

    added Footer �

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
+import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Route exact path="/" component={Home} />
 				<Route path="/about" component={About} />
 			</Switch>
+			<Footer />
 		</>
 	);
 }
