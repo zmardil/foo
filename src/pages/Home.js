cf3d2ac2fb248ce6f66cf89dd25625f25cc808f9
zmardil <btzardil@gmail.com>
2020-10-31 11:08:50 +0530

    added home with hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import heroImg from "../assets/hero-img.jfif";
 
 function Home() {
-  return (
-    <div>
-      Home Works! 
-    </div>
-  )
+	return (
+		<main className="Home">
+			<section className="Home__hero">
+				<figure>
+					<source media="(min-width: )" srcset="" sizes="" />
+					<img src={heroImg} alt="" />
+					<figcaption>
+						<h4>20% off</h4>
+						<p>Selected Home</p>
+						<Link to="/" className="cta">
+							Shop all Home Offers
+						</Link>
+					</figcaption>
+				</figure>
+			</section>
+		</main>
+	);
 }
 
 export default Home;
